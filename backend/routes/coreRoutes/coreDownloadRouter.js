7706b1e7918ce004d2322e0860d32d2e2b2c54fa
amrfahmy5 <amrfahmy3210@gmail.com>
2024-05-14 22:38:48 +0300

    print in prod - test

--- backend/routes/coreRoutes/coreDownloadRouter.js
+++ backend/routes/coreRoutes/coreDownloadRouter.js
@@ -11,10 +11,18 @@ const pug = require('pug');
 const moment = require('moment');
 let pdf = require('html-pdf');
 
+outer.route('/amr').get(async (req, res) =>{//Invoice,Quote
+  res.send("hiiii");
+  console.log("hiiiihiiiihiiii")
+
+});
 router.route('/print/:modelName/:id').get(async (req, res) =>{//Invoice,Quote
   try {
 
+  
   const { modelName,id } = req.params;
+  console.log("hooooooooooooooooooooo");
+  console.log(modelName);
   let Model =  (modelName=="quote"||modelName=="Quote")?ModelQuote:ModelInvoice;
 
   const result = await Model.findOne({ _id: id });
@@ -25,6 +33,8 @@ router.route('/print/:modelName/:id').get(async (req, res) =>{//Invoice,Quote
     res.send(html);
   }
   catch (err) {
+    console.log("asdasdasdasdasd");
+    console.log(err);
     res.send(err);
   }
 });
