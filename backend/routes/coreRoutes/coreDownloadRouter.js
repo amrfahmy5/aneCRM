459acb86f6d03d932b65935f06c941d4b2203577
amrfahmy5 <amrfahmy3210@gmail.com>
2024-05-14 22:52:33 +0300

    solve pdf path

--- backend/routes/coreRoutes/coreDownloadRouter.js
+++ backend/routes/coreRoutes/coreDownloadRouter.js
@@ -11,30 +11,18 @@ const pug = require('pug');
 const moment = require('moment');
 let pdf = require('html-pdf');
 
-router.route('/amr').get(async (req, res) =>{//Invoice,Quote
-  res.send("hiiii");
-  console.log("hiiiihiiiihiiii")
-
-});
 router.route('/print/:modelName/:id').get(async (req, res) =>{//Invoice,Quote
   try {
-
-  
   const { modelName,id } = req.params;
-  console.log("hooooooooooooooooooooo");
-  console.log(modelName);
   let Model =  (modelName=="quote"||modelName=="Quote")?ModelQuote:ModelInvoice;
-
   const result = await Model.findOne({ _id: id });
-  const html =await pug.renderFile('views/pdf/' + modelName + '.pug', {
+  const html =await pug.renderFile(process.env.projectPath +'views/pdf/' + modelName + '.pug', {
     model: result,
     moment: moment,
   });
     res.send(html);
   }
   catch (err) {
-    console.log("asdasdasdasdasd");
-    console.log(err);
     res.send(err);
   }
 });
